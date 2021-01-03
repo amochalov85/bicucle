72e475a014bd3a80ab8dd9ae8ce586b0c54b21c5
Alexandr Mochalov <Alexander.Mochalov@onlyoffice.com>
2021-01-03 14:13:48 +0300

    Distanse bicucles v.1

---
+++ bicycle.rb
@@ -0,0 +1,14 @@
+class Bicucle
+    
+    attr_accessor :number_revolutions, :diameter, :pi
+    
+def initialize (orbits = nil, meters = nil, pi = 3,14)
+    @number_revolutions = orbits
+    @diameter = meters
+    @pi
+end
+
+def  distance
+    @pi*@diameter*@number_revolutions
+end
+ 
---
+++ test_bicycle_spec.rb
@@ -0,0 +1,5 @@
+distance_bicucle  = Bicucle.new(2, 1)
+
+def sum_distance_bicucle
+  distance_bicucle.distance.eq(2,68) 
+end 
